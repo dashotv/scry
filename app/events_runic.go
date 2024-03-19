373e4a5dee73e2d4a18a530354dfa5d9ce55ee09
Shawn Catanzarite <me@shawncatz.com>
2024-03-19 02:00:25 +0000

    remove debug output

--- app/events_runic.go
+++ app/events_runic.go
@@ -3,7 +3,7 @@ package app
 import runic "github.com/dashotv/runic/app"
 
 func onRunic(app *Application, msg *runic.Release) error {
-	app.Log.Named("runic").Debugf("index: %+v", msg)
+	// app.Log.Named("runic").Debugf("index: %+v", msg)
 	_, err := app.Client.IndexRunic(msg)
 	if err != nil {
 		return err
