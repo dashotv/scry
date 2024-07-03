e518f20d4514f659acee731f8fe6d0c707b50d74
Shawn Catanzarite <me@shawncatz.com>
2024-07-03 22:50:11 +0000

    enable apm plugin

--- app/config.go
+++ app/config.go
@@ -31,6 +31,11 @@ type Config struct {
 	// Models (Database)
 	Connections ConnectionSet `env:"CONNECTIONS,required"`
 
+	// APM
+	APMServiceName string `env:"ELASTIC_APM_SERVICE_NAME,required"`
+	APMServerURL   string `env:"ELASTIC_APM_SERVER_URL,required"`
+	APMSecretToken string `env:"ELASTIC_APM_SECRET_TOKEN" envDefault:"0"`
+
 	// Router Auth
 	Auth           bool   `env:"AUTH" envDefault:"false"`
 	ClerkSecretKey string `env:"CLERK_SECRET_KEY"`
