a50d9f6bcf323d55c743750618ac8723886e785f
Shawn Catanzarite <me@shawncatz.com>
2024-07-26 15:32:10 +0000

    change runic to monthly buckets instead of daily

--- search/util.go
+++ search/util.go
@@ -7,5 +7,5 @@ import (
 
 func timeToDateBucket(t time.Time) string {
 	t = t.UTC()
-	return fmt.Sprintf("%04d%02d%02d", t.Year(), t.Month(), t.Day())
+	return fmt.Sprintf("%04d%02d", t.Year(), t.Month())
 }
